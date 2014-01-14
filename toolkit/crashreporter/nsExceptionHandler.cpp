890871a2fedd8c021db32668a195fc73862d04d8
Jed Davis <jld@mozilla.com>
2014-01-14 10:19:03 -0800

    Disable broken assertion. FIXME.

--- toolkit/crashreporter/nsExceptionHandler.cpp
+++ toolkit/crashreporter/nsExceptionHandler.cpp
@@ -2164,7 +2164,7 @@ FindPendingDir()
 static bool
 GetPendingDir(nsIFile** dir)
 {
-  MOZ_ASSERT(OOPInitialized());
+  // MOZ_ASSERT(OOPInitialized());
   if (!pendingDirectory) {
     return false;
   }
