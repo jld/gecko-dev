3404af5123550db22c6a40bd9bee0f4bfa760320
Jed Davis <jld@mozilla.com>
2014-03-10 19:49:40 -0700

    Move the seccomp filter from a header into a separate translation unit.

--- security/sandbox/linux/Sandbox.cpp
+++ security/sandbox/linux/Sandbox.cpp
@@ -20,7 +20,6 @@
 #include <pthread.h>
 
 #include "mozilla/Atomics.h"
-#include "mozilla/ArrayUtils.h"
 #include "mozilla/NullPtr.h"
 #include "mozilla/unused.h"
 #include "mozilla/dom/Exceptions.h"
@@ -37,7 +36,7 @@
 #endif
 
 #if defined(MOZ_CONTENT_SANDBOX)
-#include "seccomp_filter.h"
+#include "SandboxFilter.h"
 #include "linux_seccomp.h"
 #endif
 
@@ -56,22 +55,6 @@ static PRLogModuleInfo* gSeccompSandboxLog;
 #else
 #define LOG_ERROR(args...)
 #endif
-
-struct sock_filter seccomp_filter[] = {
-  VALIDATE_ARCHITECTURE,
-  EXAMINE_SYSCALL,
-  SECCOMP_WHITELIST,
-#ifdef MOZ_CONTENT_SANDBOX_REPORTER
-  TRAP_PROCESS,
-#else
-  KILL_PROCESS,
-#endif
-};
-
-struct sock_fprog seccomp_prog = {
-  (unsigned short)MOZ_ARRAY_LENGTH(seccomp_filter),
-  seccomp_filter,
-};
 
 /**
  * Log JS stack info in the same place as the sandbox violation
@@ -235,7 +218,9 @@ InstallSyscallFilter(void)
     return 1;
   }
 
-  if (prctl(PR_SET_SECCOMP, SECCOMP_MODE_FILTER, &seccomp_prog, 0, 0)) {
+  const sock_fprog *filter = GetSandboxFilter();
+
+  if (prctl(PR_SET_SECCOMP, SECCOMP_MODE_FILTER, (unsigned long)filter, 0, 0)) {
     return 1;
   }
   return 0;
