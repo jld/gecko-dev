040a1d21d031a0124ac949f4ba22587a7bc790d5
Jed Davis <jld@mozilla.com>
2014-09-09 16:46:32 -0700

    Fixup harder: all the base/threading

--- security/sandbox/chromium/base/threading/thread_local_android.cc
+++ security/sandbox/chromium/base/threading/thread_local_android.cc
@@ -1,10 +1,8 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "base/threading/thread_local.h"
-
-#include <windows.h>
 
 #include "base/logging.h"
 
@@ -13,27 +11,23 @@ namespace internal {
 
 // static
 void ThreadLocalPlatform::AllocateSlot(SlotType* slot) {
-  *slot = TlsAlloc();
-  CHECK_NE(*slot, TLS_OUT_OF_INDEXES);
+  bool succeed = slot->Initialize(NULL);
+  CHECK(succeed);
 }
 
 // static
 void ThreadLocalPlatform::FreeSlot(SlotType slot) {
-  if (!TlsFree(slot)) {
-    NOTREACHED() << "Failed to deallocate tls slot with TlsFree().";
-  }
+  slot.Free();
 }
 
 // static
 void* ThreadLocalPlatform::GetValueFromSlot(SlotType slot) {
-  return TlsGetValue(slot);
+  return slot.Get();
 }
 
 // static
 void ThreadLocalPlatform::SetValueInSlot(SlotType slot, void* value) {
-  if (!TlsSetValue(slot, value)) {
-    LOG(FATAL) << "Failed to TlsSetValue().";
-  }
+  slot.Set(value);
 }
 
 }  // namespace internal
