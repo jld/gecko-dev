2ed9eb23bda65415622ed2bc7628988e8bac15a2
Jed Davis <jld@mozilla.com>
2014-10-09 17:25:15 -0700

    Revert "Bug 1080803 - Tighten up AES decryption in ClearKey CDM - r=cpearce"
    This reverts commit 31c1037624b0e94cd77b48a0c53564ebd5f33f49.

--- media/gmp-clearkey/0.1/ClearKeyUtils.cpp
+++ media/gmp-clearkey/0.1/ClearKeyUtils.cpp
@@ -2,7 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include <algorithm>
 #include <assert.h>
 #include <ctype.h>
 #include <stdarg.h>
@@ -63,9 +62,7 @@ ClearKeyUtils::DecryptAES(const vector<uint8_t>& aKey,
     vector<uint8_t> enc(encLen);
     oaes_encrypt(aes, &aIV[0], CLEARKEY_KEY_LEN, &enc[0], &encLen);
 
-    assert(encLen >= 2 * OAES_BLOCK_SIZE + CLEARKEY_KEY_LEN);
-    size_t blockLen = std::min(aData.size() - i, CLEARKEY_KEY_LEN);
-    for (size_t j = 0; j < blockLen; j++) {
+    for (size_t j = 0; j < CLEARKEY_KEY_LEN; j++) {
       aData[i + j] ^= enc[2 * OAES_BLOCK_SIZE + j];
     }
     IncrementIV(aIV);
