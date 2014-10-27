0af4b09a1d8a71019bf2177f73b40c537391c04c
Jed Davis <jld@mozilla.com>
2014-10-27 14:47:43 -0700

    Bug 1132760: Move PR_DuplicateEnvironment to NSPR.
    This also changes non-Android/B2G Linux to use PR_DuplicateEnvironment
    and execve instead of mutating the child process's environment after fork;
    this makes the fork->exec path async signal safe there.

--- nsprpub/pr/src/misc/prenv.c
+++ nsprpub/pr/src/misc/prenv.c
@@ -5,6 +5,7 @@
 
 #include <string.h>
 #include "primpl.h"
+#include "prmem.h"
 
 /* Lock used to lock the environment */
 #if defined(_PR_NO_PREEMPT)
@@ -64,4 +65,27 @@ PR_IMPLEMENT(PRStatus) PR_SetEnv(const char *string)
     result = _PR_MD_PUT_ENV(string);
     _PR_UNLOCK_ENV();
     return (result)? PR_FAILURE : PR_SUCCESS;
+}
+
+PR_IMPLEMENT(char **) PR_DuplicateEnvironment(void)
+{
+    char **result = NULL;
+    char **s;
+    char **d;
+
+    _PR_LOCK_ENV();
+    for (s = environ; *s != NULL; s++)
+      ;
+    if ((result = (char **)PR_Malloc(sizeof(char *) * (s - environ + 1))) != NULL) {
+      for (s = environ, d = result; *s != NULL; s++, d++) {
+        size_t len;
+
+        len = strlen(*s) + 1;
+        *d = PR_Malloc(len);
+        memcpy(*d, *s, len);
+      }
+      *d = NULL;
+    }
+    _PR_UNLOCK_ENV();
+    return result;
 }
