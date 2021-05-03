aefd7bdbfb6c226847bcc922e528e8a805c88fe4
Jed Davis <jld@mozilla.com>
2021-05-03 14:35:14 -0600

    INTENTIONAL BUG, DO NOT LAND

--- dom/ipc/ContentChild.cpp
+++ dom/ipc/ContentChild.cpp
@@ -711,12 +711,14 @@ bool ContentChild::Init(MessageLoop* aIOLoop, base::ProcessId aParentPid,
   // This must be checked before any IPDL message, which may hit sentinel
   // errors due to parent and content processes having different
   // versions.
+#if 0
   MessageChannel* channel = GetIPCChannel();
   if (channel && !channel->SendBuildIDsMatchMessage(aParentBuildID)) {
     // We need to quit this process if the buildID doesn't match the parent's.
     // This can occur when an update occurred in the background.
     ProcessChild::QuickExit();
   }
+#endif
 
 #if defined(__OpenBSD__) && defined(MOZ_SANDBOX)
   StartOpenBSDSandbox(GeckoProcessType_Content);
