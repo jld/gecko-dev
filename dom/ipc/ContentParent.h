9a4605056b8d6c999db58b3f7c024d402ca3eb93
Jed Davis <jld@mozilla.com>
2021-05-03 18:40:29 -0600

    Bug 1695381 - Use IPC for content processes' moz-icon URL loading. r?tnikkel
    This sends the icon data inline as a `ByteBuf`, because the largest
    possible size (255x255) occupies 255kB, and the more typical size of
    16x16 is only 1kB.  Given that IPC throughput is on the order of 1GB/s,
    it's not necessary to try to send the data chunked or out-of-band, and
    in typical use it would just add overhead.

--- dom/ipc/ContentParent.h
+++ dom/ipc/ContentParent.h
@@ -1412,6 +1412,9 @@ class ContentParent final
       const MaybeDiscardedBrowsingContext& aContainerContext,
       FeaturePolicy* aContainerFeaturePolicy);
 
+  mozilla::ipc::IPCResult RecvGetSystemIcon(nsIURI* aURI,
+                                            GetSystemIconResolver&& aResolver);
+
  public:
   void SendGetFilesResponseAndForget(const nsID& aID,
                                      const GetFilesResponseResult& aResult);
