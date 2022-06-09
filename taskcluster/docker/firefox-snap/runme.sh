827bf1869f37f16426e094467505118aeee2a28b
Jed Davis <jld@mozilla.com>
2022-06-08 19:02:56 -0600

    Parameterize the snapcraft.yaml on the app name.

--- taskcluster/docker/firefox-snap/runme.sh
+++ taskcluster/docker/firefox-snap/runme.sh
@@ -4,6 +4,7 @@ set -xe
 
 # Thunderbird Snap builds will set this to "thunderbird"
 : PRODUCT                       "${PRODUCT:=firefox}"
+: APP_NAME                      "${APP_NAME:=$PRODUCT}"
 
 # Required env variables
 test "$VERSION"
@@ -74,7 +75,10 @@ done
 cp -v "$SCRIPT_DIRECTORY/tmpdir" "$SOURCE_DEST"
 
 # Generate snapcraft manifest
-sed -e "s/@VERSION@/${VERSION}/g" -e "s/@BUILD_NUMBER@/${BUILD_NUMBER}/g" ${PRODUCT}.snapcraft.yaml.in > "${WORKSPACE}/snapcraft.yaml"
+sed -e "s/@VERSION@/${VERSION}/g" \
+    -e "s/@BUILD_NUMBER@/${BUILD_NUMBER}/g" \
+    -e "s/@APP_NAME@/${APP_NAME}/g" \
+    ${PRODUCT}.snapcraft.yaml.in > "${WORKSPACE}/snapcraft.yaml"
 cd "${WORKSPACE}"
 
 # Make sure snapcraft knows we're building amd64, even though we may not be on this arch.
